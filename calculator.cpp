0bc8f4d27759dd38c1603743eb023f1d051ef8e6
Kode1n-KPI <kusnirenkovlad3@gmail.com>
2025-11-03 19:06:28 +0200

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,24 @@
+#include "calculator.h"
+
+int Calculator::Add (double a, double b)
+{
+ return a + b + 0.5;
+}
+
+int Calculator::Sub (double a, double b)
+{
+ return Add (a, -b);
+}
+
+int Calculator::Mul (double a, double b)
+{
+ return a * b + 0.5;
+}
+
+int Calculator::Div (double a, double b) 
+{
+ if (b == 0) {
+  return 0; 
+ }
+ return (a / b) + 0.5; 
+}
